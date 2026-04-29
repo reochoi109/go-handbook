7e4a0ac6415173821b23998c056e7a030edb1e8c
reochoi109 <reochoi109@gmail.com>
2026-04-29 23:43:40 +0900

    feat(testing): implement runnable testing/example server

--- testing/example/main.go
+++ testing/example/main.go
@@ -1,3 +1,54 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/reochoi109/go-handbook/testing/example/internal/domain"
+	"github.com/reochoi109/go-handbook/testing/example/internal/service"
+	"github.com/reochoi109/go-handbook/testing/example/internal/web"
+	"github.com/reochoi109/go-handbook/testing/example/repository"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	repo := repository.NewInMemoryUserRepo([]domain.User{
+		{ID: "user_123", Name: "Reo"},
+		{ID: "user_456", Name: "Alex"},
+	})
+	svc := service.NewUserService(repo)
+	h := web.NewHandler(svc)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/users/", h.GetUser)
+
+	srv := &http.Server{
+		Addr:              ":8080",
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		<-ctx.Done()
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(shutdownCtx); err != nil {
+			log.Printf("shutdown error: %v", err)
+		}
+	}()
+
+	log.Println("listening on :8080")
+	log.Println(`try: curl -i localhost:8080/users/user_123`)
+
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Println("server error:", err)
+		os.Exit(1)
+	}
+}
